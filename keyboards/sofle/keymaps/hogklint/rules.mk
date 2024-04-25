44dbc68c545dac41b67f04892cc7d67439c8aa6b
Jimmie Högklint <hogklint@gmail.com>
2024-04-25 21:31:39 +0200

    Convert Sofle build to elite_pi controller

--- keyboards/sofle/keymaps/hogklint/rules.mk
+++ keyboards/sofle/keymaps/hogklint/rules.mk
@@ -1,5 +1,7 @@
-
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes
 CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
+
+# Seems to an Elite Pi controller on it instead of the ProMicro specified
+CONVERT_TO=elite_pi
